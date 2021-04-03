7f8f47c0aa720b9e8985fa9920d09a946ab2ffb9
Valikduplii <69109832+Valikduplii@users.noreply.github.com>
2021-04-03 18:42:36 +0300

    Add files via upload

--- javaScript/JavaScript.js
+++ javaScript/JavaScript.js
@@ -1,6 +1,42 @@
 $(document).ready(function(){
-    $('.header__burger').click(function(event){
-       $('.header__burger, .header__menu').toggleClass('active');
-       $('body').toggleClass('lock');
-    })
- })
+   $('.header__burger').click(function(event){
+      $('.header__burger, .header__menu').toggleClass('active');
+      $('body').toggleClass('lock');
+   })
+   if ($('.news__slider').length > 0) {
+      $('.news__slider').slick({
+         easing: 'ease',
+         autoplay: false,
+         dots: true,
+         slidesToShow: 2,
+         autoplaySpeed: 10000,
+         rows: 1,
+         pauseOnFocus: true,
+         pauseOnHover: true,
+         slidesToScroll: 1,
+         infinite: false,
+         adaptiveHeight: true,
+         
+         // responsive: [
+         //    {
+         //       breakpoint: 1280,
+         //       settings: {
+         //          slidesToShow: 2,
+         //       }
+         //    }, {
+         //       breakpoint: 992,
+         //       settings: {
+         //          slidesToShow: 1,
+         //       }
+         //    }
+         // ],
+      });
+      // $(".news__more" || ".news__more.active").click(function () {
+      //    $(this).parents(".news__item").find(".news__text").toggleClass("active");
+      //    $(".news__more").toggleClass("active");
+      // })
+      // $(".small-img").click(function() {
+      //    $(".big-img").attr("src", $(this).attr("src"));
+      // });
+   }
+})
